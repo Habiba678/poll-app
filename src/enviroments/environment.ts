e30d8e5efe277daa9bfe6e1cf1bd2c1a11ee0e41
Habiba678 <hananhussain12@hotmail.com>
2026-09-07 17:15:49 +0200

    Add Supabase

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://zgwuivkwmixnjumrycdk.supabase.co',
+    supabaseKey: 'sb_publishable_igDoeQPjHHH4NojPlqCa5A_cj_eJCf8'
+  };
